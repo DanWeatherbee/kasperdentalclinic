ec2364c29d9eb1a36e7daea231bb4f365e4dd093
Dan Weatherbee <dan@jedi-clan.us>
2017-06-02 05:24:15 -0700

    data: added correct information for clients site.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,1 @@
-var app = function () {
-    $('#p-1').prepend('<option>' + $('#src-file').val() + '</option>')
-    var srcFile = $('#p-1 option:selected').val();
-    $('#header-img').attr("src", srcFile);
 
-}
