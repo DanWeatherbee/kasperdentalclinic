dd27d28aca652d313de78c431632504e1581d87d
Dan Weatherbee <dan@jedi-clan.us>
2017-06-15 13:28:04 -0700

    style: amazing effects acheived with gradients.

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,1 @@
-$(window).scroll(function () {
-    $('#header-img').fadeOut();
-});
 
-$('#header-img').click(function () {
-    $('#header-img').fadeOut();
-});
-$(window).scroll(function () {
-    $('#con1').fadeOut();
-});
-
-$('#content1').click(function () {
-    $('#con1').fadeOut();
-
-});
