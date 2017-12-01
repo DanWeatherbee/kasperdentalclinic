d313264159bd155e39193aee268968117ed6660d
Dan Weatherbee <dan@jedi-clan.us>
2017-12-01 05:45:01 -0800

    feat:created new app object for site and data.

--- src/js/core/db.js
+++ src/js/core/db.js
@@ -33,29 +33,3 @@ db.display = function() {
 
 };
 db.display();
-
-
-
-var scriptArray = [
-
-    "js/core/lib/tether.min.js",
-    "js/core/lib/bootstrap.min.js",
-    "js/core/lib/mdb.min.js",
-    "js/core/lib/popper.min.js",
-    "js/core/helmet/helmet.js",
-    "js/core/navbar/nav.js",
-    "js/core/admin/controls/add.js",
-    "js/core/admin/controls/get.js",
-    "js/core/admin/controls/save.js",
-    "js/core/admin/controls/clear.js",
-    "js/core/admin/admin.js"
-];
-
-var appendScripts = function(src) {
-    this.src = src;
-    var x = document.createElement("script");
-    x.src = this.src;
-    document.body.append(x);
-}
-
-scriptArray.forEach(function(script) { appendScripts(script); });
